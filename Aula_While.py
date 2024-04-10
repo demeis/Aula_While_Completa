794c5ba0b70cf4b49758d0c571c3c1179e4d5035
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-10 20:50:20 -0300

    Initial commit

---
+++ Aula_While.py
@@ -0,0 +1,50 @@
+# x = 1
+# while x <= 100:
+#     print(x)
+#     x += 1
+
+# x = 50
+# while x <= 100:
+#     print(x)
+#     x += 1
+
+import time
+
+# x = 10
+# while x >= 0:
+#     print(x)
+#     time.sleep(1)
+#     x -= 1
+# print("Fogo!")
+
+# inicio = int(input("Digite o primeiro número:"))
+# fim = int(input("Digite o último número:"))
+# if fim < 0:
+#     while inicio >= fim:
+#         print(inicio)
+#         inicio -= 1
+# else:
+#     while inicio <= fim:
+#         print(inicio)
+#         inicio += 1
+
+# x = 1
+# fim = int(input("Digite o último número:"))
+# while x <= fim:
+#          print(x)
+#          x += 2
+
+# x = 3
+# while x <= 3*10:
+#     print(x)
+#     x += 3
+
+# n = int(input("Digite sua tabuada:"))
+# x = 1
+# while x <= 10:
+#     print(n*x)
+#     x += 1
+
+
+
+
